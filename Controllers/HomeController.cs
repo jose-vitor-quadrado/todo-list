7ed139f7c3dd0ea7a9aad7b016b4c49963ccdd99
Jose Vitor Quadrado <josevitorquadrado@gmail.com>
2022-11-07 14:31:39 -0300

    added update and delete todos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,12 @@ namespace Todo.Controllers
             return context.Todos.ToList();
         }
 
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
         [HttpPost("/")]
         public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext context)
         {
@@ -20,6 +26,36 @@ namespace Todo.Controllers
             context.SaveChanges();
 
             return todo;
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+            [FromRoute] int id, 
+            [FromBody] TodoModel todo, 
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null) 
+                return todo;
+            
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            
+            return model;
+        }
+
+        [HttpDelete("/{id:int}")]
+        public TodoModel Delete([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return model;
         }
     }
 }
